29d9b48af530c13f281af31ccd571dd1962a6556
Álvaro BM <85340857+bm-alvaro@users.noreply.github.com>
2024-07-26 18:21:29 +0200

    organising scripts to modularize

--- R/global.R
+++
@@ -1,9 +0,0 @@
-library(shiny)
-library(leaflet)
-library(ggplot2)
-library(dplyr)
-library(shinythemes)
-library(shinycssloaders)
-
-poland_data <- read.csv("C:/Users/Alvaro/Desktop/Alvaro/Scripts/Appsilon/ShinyDevTask/data/poland_data.csv")
-multimedia_data <- read.csv("C:/Users/Alvaro/Desktop/Alvaro/Scripts/Appsilon/ShinyDevTask/data/multimedia_data.csv")
